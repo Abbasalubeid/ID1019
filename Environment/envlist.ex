677f7b24e96f2573a8552399b914f4b197b2bcdc
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-24 21:18:40 +0100

    Lookup and remove functions

--- Environment/envlist.ex
+++ Environment/envlist.ex
@@ -4,9 +4,25 @@ defmodule Envlist do
 
   def add(map, key, value) do
     case map do
-      [] -> map ++ [{key, value}]
+      [] -> [{key, value}]
       [{k, v} | rest] when k == key -> [{key, value} | rest]
       [first | rest] -> [first | add(rest, key, value)]
+    end
+  end
+
+  def lookup(map, key) do
+    case map do
+      [] -> nil
+      [{k, v} | rest] when k == key -> {k, v}
+      [first | rest] -> lookup(rest, key)
+    end
+  end
+
+  def remove(key, map) do
+    case map do
+      [] -> []
+      [{k, v} | tail] when k == key -> tail
+      [head | tail] -> [head | remove(key, tail)]
     end
   end
 
