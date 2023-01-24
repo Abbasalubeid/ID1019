bddbbb154fc44f115b6edf4d15044c89541d7bc3
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-24 22:01:55 +0100

    Benchmarks

--- Environment/bench.ex
+++ Environment/bench.ex
@@ -1,30 +1,4 @@
-defmodule EnvList do
-
-  def new() do [] end
-
-  def add(map, key, value) do
-    case map do
-      [] -> [{key, value}]
-      [{k, v} | rest] when k == key -> [{key, value} | rest]
-      [first | rest] -> [first | add(rest, key, value)]
-    end
-  end
-
-  def lookup(map, key) do
-    case map do
-      [] -> nil
-      [{k, v} | rest] when k == key -> {k, v}
-      [first | rest] -> lookup(rest, key)
-    end
-  end
-
-  def remove(key, map) do
-    case map do
-      [] -> []
-      [{k, v} | tail] when k == key -> tail
-      [head | tail] -> [head | remove(key, tail)]
-    end
-  end
+defmodule Bench do
 
   def bench(i, n) do
     seq = Enum.map(1..i, fn(_) -> :rand.uniform(i) end)
@@ -59,5 +33,6 @@ defmodule EnvList do
     :io.format("~6.w~12.2f~12.2f~12.2f\n", [i, tla/n, tll/n, tlr/n])
     end)
     end
+
 
 end
