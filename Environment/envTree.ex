1c93ab756eccf6162a5ffd882189846d79c4ef4b
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-25 22:33:57 +0100

    Remove works for all special cases

--- Environment/envTree.ex
+++ Environment/envTree.ex
@@ -33,20 +33,57 @@ defmodule EnvTree do
     {:node, k, v, add(left, key, value), right}
   end
 
+  #New structure based on the one we are adding
   def add({:node, k, v, left, right}, key, value) do
     {:node, k, v, left, add(right, key, value)}
   end
 
   def lookup(_, :nil) do :not_found end
 
-  def lookup(key, {:node, key, value, _, _}) do {:value, value} end
+  def lookup(key, {:node, key, value, _, _}) do  value end
 
+  #Since the key is sorted, we can choose to go left or right depending on 'key'
   def lookup(key, {:node, k, _, left, right}) do
     if key < k do
       lookup(key, left)
     else
       lookup(key, right)
     end
+  end
+
+  def remove(nil, _) do :nil end
+
+  #If the key to remove is the root and there is no left node
+  #Remove the root and keep right
+  def remove({:node, key, _, nil, right}, key) do right end
+
+  #Same as above, but if there is no right node
+  def remove({:node, key, _, left, nil}, key) do left end
+
+  #If the root has left and right
+  #Choose a successor, in this case the node with the minimum value
+  #from the right tree.
+  def remove({:node, key, _, left, right}, key) do
+    minimum_from_right = leftmost(right)
+    {:node, elem(minimum_from_right, 0), elem(minimum_from_right, 1), left, elem(minimum_from_right, 2)}
+  end
+
+  def remove({:node, k, v, left, right}, key) when key < k do
+  {:node, k, v, remove(left, key), right}
+  end
+
+  def remove({:node, k, v, left, right}, key) do
+  {:node, k, v, left, remove(right, key)}
+  end
+
+  #When we are at the leftmost node
+  def leftmost({:node, key, value, nil, rest}) do
+    {key, value, rest}
+  end
+
+  def leftmost({:node, k, v, left, right}) do
+    last = leftmost(left)
+    {elem(last, 0), elem(last, 1), {:node, k, v, elem(last, 2), right}}
   end
 
 end
