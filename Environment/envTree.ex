46863bfa20f86cf06971a8c2ba02c1a8ef9864e2
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-25 18:13:13 +0100

    Check if an element exists in a tree (lecture trees-3)

--- Environment/envTree.ex
+++ Environment/envTree.ex
@@ -1,3 +1,21 @@
 defmodule EnvTree do
+  # @type leaf() :: {:leaf, value}
+  # @type node() :: {:node, value, left, right}
+
+  # treeExample() :: {:node, :rootValue, {:leaf, :b} {:leaf, :c}}
+
+  def member(_, :nil) do :no end
+  def member(elem, {:leaf, elem}) do :yes end
+  def member(_, {:leaf, _}) do :no end
+  def member(elem, {:node, elem, _, _}) do :yes end
+  def member(elem, {:node, _, left, right}) do
+    case member(elem, left) do
+      :yes -> :yes
+      :no -> case member(elem, right) do
+              :yes -> :yes
+              :no -> :no
+             end
+    end
+  end
 
 end
