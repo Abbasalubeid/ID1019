780a816f3dec57815155dc8d678d4638d834211b
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-26 13:23:39 +0100

    Benchmarks for tree

--- Environment/envTree.ex
+++ Environment/envTree.ex
@@ -86,4 +86,36 @@ defmodule EnvTree do
     {elem(last, 0), elem(last, 1), {:node, k, v, elem(last, 2), right}}
   end
 
+  def bench(i, n) do
+    seq = Enum.map(1..i, fn(_) -> :rand.uniform(i) end)
+    list = Enum.reduce(seq, add(:nil, :rand.uniform(i), :value), fn(e, list) -> add(list, e, :foo)end)
+    seq = Enum.map(1..n, fn(_) -> :rand.uniform(i) end)
+    {add, _} = :timer.tc(fn() ->
+    Enum.each(seq, fn(e) ->
+    add(list, e, :foo)
+              end)
+            end)
+    {lookup, _} = :timer.tc(fn() ->
+    Enum.each(seq, fn(e) ->
+    lookup(list, e)
+            end)
+          end)
+    {remove, _} = :timer.tc(fn() ->
+    Enum.each(seq, fn(e) ->
+    remove(list, e)
+            end)
+          end)
+    {i, add, lookup, remove}
+  end
+
+  def bench(n) do
+    ls = [16,32,64,128,256,512,1024,2*1024,4*1024,8*1024]
+    :io.format("# benchmark with ~w operations, time per operation in us\n", [n])
+    :io.format("~6.s~12.s~12.s~12.s\n", ["n", "add", "lookup", "remove"])
+    Enum.each(ls, fn (i) ->
+    {i, tla, tll, tlr} = bench(i, n)
+    :io.format("~6.w~12.2f~12.2f~12.2f\n", [i, tla/n, tll/n, tlr/n])
+            end)
+  end
+
 end
