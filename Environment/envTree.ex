a7aeafd7110e753a56c60ce9b00e16c14a90177b
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-26 19:06:39 +0100

    Changed the benchmarks nanoseconds

--- Environment/envTree.ex
+++ Environment/envTree.ex
@@ -90,27 +90,33 @@ defmodule EnvTree do
     seq = Enum.map(1..i, fn(_) -> :rand.uniform(i) end)
     list = Enum.reduce(seq, add(:nil, :rand.uniform(i), :value), fn(e, list) -> add(list, e, :foo)end)
     seq = Enum.map(1..n, fn(_) -> :rand.uniform(i) end)
-    {add, _} = :timer.tc(fn() ->
+    start_time = :erlang.monotonic_time()
     Enum.each(seq, fn(e) ->
     add(list, e, :foo)
               end)
-            end)
-    {lookup, _} = :timer.tc(fn() ->
+    end_time = :erlang.monotonic_time()
+    add_time = end_time - start_time
+
+    start_time = :erlang.monotonic_time()
     Enum.each(seq, fn(e) ->
     lookup(list, e)
             end)
-          end)
-    {remove, _} = :timer.tc(fn() ->
+    end_time = :erlang.monotonic_time()
+    lookup_time = end_time - start_time
+
+    start_time = :erlang.monotonic_time()
     Enum.each(seq, fn(e) ->
     remove(list, e)
             end)
-          end)
-    {i, add, lookup, remove}
+    end_time = :erlang.monotonic_time()
+    remove_time = end_time - start_time
+
+    {i, add_time, lookup_time, remove_time}
   end
 
   def bench(n) do
     ls = [16,32,64,128,256,512,1024,2*1024,4*1024,8*1024]
-    :io.format("# benchmark with ~w operations, time per operation in us\n", [n])
+    :io.format("# benchmark with ~w operations, time per operation in ns\n", [n])
     :io.format("~6.s~13.s~17.s~16.s\n", ["n", "add", "lookup", "remove"])
     Enum.each(ls, fn (i) ->
     {i, tla, tll, tlr} = bench(i, n)
