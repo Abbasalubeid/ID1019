3622b484bd15fb354da0de8e01f191ba2ccda2bc
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-27 14:53:25 +0100

    Comment on the add/3 function

--- Environment/envTree.ex
+++ Environment/envTree.ex
@@ -35,7 +35,7 @@ defmodule EnvTree do
     {:node, k, v, add(left, key, value), right}
   end
 
-  #New structure based on the one we are adding
+  #Same as above but when key > k
   def add({:node, k, v, left, right}, key, value) do
     {:node, k, v, left, add(right, key, value)}
   end
