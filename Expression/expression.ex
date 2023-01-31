916ad53e702c4d5e2f68f7356aac2950201b9b28
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-31 12:45:55 +0100

    Done with the multiplication and division evaulations

--- Expression/expression.ex
+++ Expression/expression.ex
@@ -23,12 +23,28 @@ defmodule Expression do
     sub(eval(e1, env), eval(e2, env))
   end
 
+  def eval({:mul, e1, e2}, env) do
+    mul(eval(e1, env), eval(e2, env))
+  end
+
+  def eval({:div, e1, e2}, env) do
+    divide(eval(e1, env), eval(e2, env))
+  end
+
   def add(e1, e2) do
     e1 + e2
   end
 
   def sub(e1, e2) do
     e1 - e2
+  end
+
+  def mul(e1, e2) do
+    e1 * e2
+  end
+
+  def divide(e1, e2) do
+    e1 / e2
   end
 
 
