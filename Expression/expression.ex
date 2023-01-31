57dd973222b4675f19071233728084ee7af33286
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-31 20:23:22 +0100

    Removed unused code

--- Expression/expression.ex
+++ Expression/expression.ex
@@ -1,15 +1,5 @@
 defmodule Expression do
 
-  # @type literal() :: {:num, n}
-  #                 | {:var, a}
-  #                 | {:q, n, m}
-
-  # # @type expr() :: {:add, expr(), expr()}
-  # #               | {:sub, expr(), expr()}
-  # #               | {:mul, expr(), expr()}
-  # #               | {:div, expr(), expr()}
-  # #               | literal()
-
   #If it is just a number, no variable mapping is needed
   def eval({:num, n}, _) do n end
   #Get the value of the variable
