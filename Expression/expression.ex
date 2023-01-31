8c44a3445f63f6de2ea113e151ec7c8d021cd822
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-31 11:27:32 +0100

    Documentation about a 'literal'

--- Expression/expression.ex
+++ Expression/expression.ex
@@ -1,5 +1,8 @@
 defmodule Expression do
 
+@type literal() :: {:num, n}
+                 | {:var, a}
+                 | {:q, n, m}
 
 @type expr() :: {:add, expr(), expr()}
               | {:sub, expr(), expr()}
