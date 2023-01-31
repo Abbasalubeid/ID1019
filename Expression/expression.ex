e7d946a3426325c314c2a737d1dd1c99df45b574
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-01-31 12:35:10 +0100

    Done with the add/2 function

--- Expression/expression.ex
+++ Expression/expression.ex
@@ -1,12 +1,27 @@
 defmodule Expression do
 
-@type literal() :: {:num, n}
-                 | {:var, a}
-                 | {:q, n, m}
+  # @type literal() :: {:num, n}
+  #                 | {:var, a}
+  #                 | {:q, n, m}
 
-@type expr() :: {:add, expr(), expr()}
-              | {:sub, expr(), expr()}
-              | {:mul, expr(), expr()}
-              | {:div, expr(), expr()}
-              | literal()
+  # # @type expr() :: {:add, expr(), expr()}
+  # #               | {:sub, expr(), expr()}
+  # #               | {:mul, expr(), expr()}
+  # #               | {:div, expr(), expr()}
+  # #               | literal()
+
+  #If it is just a number, no variable mapping is needed
+  def eval({:num, n}, _) do n end
+  #Get the value of the variable
+  def eval({:var, v}, env) do Map.get(env, v) end
+
+  def eval({:add, e1, e2}, env) do
+    add(eval(e1, env), eval(e2, env))
+  end
+
+  def add(e1, e2) do
+    e1 + e2
+  end
+
+
 end
