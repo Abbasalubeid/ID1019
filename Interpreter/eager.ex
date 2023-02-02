89db93a415b95d3bd9a773239a8731166a1ddabf
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-02-02 19:55:18 +0100

    Pattern matching works according to the tests in the assignment

--- Interpreter/eager.ex
+++ Interpreter/eager.ex
@@ -15,8 +15,8 @@ defmodule Eager do
     end
   end
 
-  #If it is a tuple, check the first variable and then the other,
-  #only if the first is != error
+  #Compound expression, evaluate the first variable and then the other,
+  #only if the first is != error/bottoms
   def eval_expr({:cons, var1, var2}, env) do
     case eval_expr(var1, env) do
       :error ->
@@ -31,6 +31,41 @@ defmodule Eager do
         end
     end
   end
+
+  #Matching an atom to its corresponding data structure gives a match
+  def eval_match({:atm, a}, a, env) do {:ok, env} end
+
+  #A don't care always gives a match
+  def eval_match(:ignore, _, env) do {:ok, env} end
+
+  #Matching a variable with a structure can give different results
+  def eval_match({:var, v}, str, env) do
+    case Env.lookup(v, env) do
+      nil ->
+        {:ok, Env.add(v, str, env)}
+      #The exact same va
+      {^v, ^str} ->
+        {:ok, env}
+
+      #Anything else gives a wrong env
+      {_, _} ->
+        :fail
+    end
+  end
+
+  #Match the first expression
+  def eval_match({:cons, var1, var2}, {s1, s2}, env) do
+    case eval_match(var1, s1, env) do
+      :fail ->
+        :fail
+      #Match the second expression with the new env
+      {:ok, env} ->
+        eval_match(var2, s2, env)
+    end
+  end
+
+
+
 
 
 end
