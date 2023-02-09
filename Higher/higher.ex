765c11ad087df47fe3a3d2edaa99791b58653630
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-02-09 19:21:15 +0100

    Done with the first/! function

--- Higher/higher.ex
+++ Higher/higher.ex
@@ -1,7 +1,9 @@
 defmodule Higher do
 
-  def double ([]) do [] end
+  def double([]) do [] end
+  def double([first | rest]) do [2 * first | double(rest)] end
 
-  def double ([h | t]) do [2 * h | double(t)] end
+  def five([]) do [] end
+  def five([first | rest]) do [first + 5 | five(rest)] end
 
 end
