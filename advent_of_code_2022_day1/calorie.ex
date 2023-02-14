a940446c072751fef31168c73a7eb98acb1ee2af
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-02-14 21:16:09 +0100

    An add function for all calories of one Elf

--- advent_of_code_2022_day1/calorie.ex
+++ advent_of_code_2022_day1/calorie.ex
@@ -2,6 +2,21 @@ defmodule Calorie do
 
   def reader do
     records = File.read!("input.txt")
-    records
+    #There is a "\n\n" between each elf
+    elfs = String.split(records, "\n\n")
+    elfs
   end
+
+  def add(string) do
+    #There is a "\n" between each number of calories for one elf
+    add(String.split(string, "\n"), 0)
+  end
+
+  def add([], acc) do acc end
+
+  def add([head | tail], acc) do
+    add(tail, String.to_integer(head) + acc)
+  end
+
+
 end
