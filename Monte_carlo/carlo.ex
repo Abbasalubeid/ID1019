3d22852c4e9a7ad0830f09e7e526fda1e5c52f1f
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-02-19 18:38:02 +0100

    Typo

--- Monte_carlo/carlo.ex
+++ Monte_carlo/carlo.ex
@@ -1,10 +1,10 @@
 defmodule Carlo do
+
   def dart(r) do
     x = Enum.random(0..r)
     y = Enum.random(0..r)
     #If r^2 > x^2 + y^2 -> (x,y) is inside
-    :math.pow(r, 2)  >  :math.pow(x, 2) + :math.pow(x, 2)
+    :math.pow(r, 2)  >  :math.pow(x, 2) + :math.pow(y, 2)
   end
-
 
 end
