704551aeec372ddc133629a0c1ce30dab4b44802
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-03-01 14:32:46 +0100

    take/2 and drop/2 should only work with positive numbers

--- Train_shunting/train.ex
+++ Train_shunting/train.ex
@@ -2,12 +2,16 @@ defmodule Train do
 
   def take(_, 0) do [] end
   def take([first | rest ], n) do
+    if n > 0 do
     [first | take(rest, n - 1)]
+    end
   end
 
   def drop(train, 0) do train end
   def drop([first | rest ], n) do
-    drop(rest, n-1)
+    if n > 0 do
+      drop(rest, n-1)
+    end
   end
 
   def append(train1, train2) do
